3fc47bed133d9402a5c52e5e67fb4e7643a668de
Jhosep Tinta <tmjhosep@gmail.com>
2023-11-16 23:48:31 -0400

    changes

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const listServices = [
     {
         name: 'Seguridad Electronica',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae inventore ipsam autem. Eveniet fuga aliquam ullam ad vero impedit. Id fuga ipsum saepe quasi incidunt eos molestiae libero facere!',
+        description: 'Vamos de la mano ofreciendote la mejor solución del mercado, trabajando conjuntamente con la marca HIKVISION. En el mundo de las alarmas de seguridad contamos con un gran aliado para brindarte la solución inalambria y alambrica, con llamada en tiempo real',
         image: 'image1.jpg'
     },
     {
         name: 'Domotica',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae inventore ipsam autem. Eveniet fuga aliquam ullam ad vero impedit. Id fuga ipsum saepe quasi incidunt eos molestiae libero facere!',
+        description: 'Transforma tu hogar con nuestro servicio de instalaciones domóticas. Contamos con una amplia variedad de dispositivos que pueden mejorar tu día a día, brindándote comodidad, eficiencia y control total sobre tu entorno. Desde sistemas de iluminación inteligente hasta termostatos automatizados, personalizamos soluciones para hacer tu vida más fácil y conectada.',
         image: 'image1.jpg'
     },
     {
         name: 'Electricidad Domiciliaria',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae inventore ipsam autem. Eveniet fuga aliquam ullam ad vero impedit. Id fuga ipsum saepe quasi incidunt eos molestiae libero facere!',
+        description: 'Optimiza tu hogar con nuestro servicio de electricidad domiciliaria. Ofrecemos soluciones seguras y eficientes para garantizar un suministro eléctrico confiable. Desde instalaciones y reparaciones hasta actualizaciones de sistemas, nuestro equipo experto se encarga de todo. Contamos con las mejores prácticas y tecnologías para asegurar que tu hogar esté siempre conectado de manera segura y eficiente.',
         image: 'image1.jpg'
     },
     {
         name: 'Redes',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae inventore ipsam autem. Eveniet fuga aliquam ullam ad vero impedit. Id fuga ipsum saepe quasi incidunt eos molestiae libero facere!',
+        description: 'Garantizamos una conectividad segura y confiable en tu comercio o domicilio mediante tecnología avanzada y cables de alta calidad, asegurando que siempre estés conectado de manera segura.',
         image: 'image1.jpg'
     }
 ];
