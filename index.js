fd856cfeda3e7cec0a2bb55aa056e4813ff439c2
Jhosep Tinta <tmjhosep@gmail.com>
2024-02-12 12:24:57 -0400

    add images

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const listServices = [
     {
         name: 'Seguridad Electronica',
         description: 'Vamos de la mano ofreciendote la mejor solución del mercado, trabajando conjuntamente con la marca HIKVISION. En el mundo de las alarmas de seguridad contamos con un gran aliado para brindarte la solución inalambria y alambrica, con llamada en tiempo real',
-        image: 'image1.jpg'
+        image: './assets/images/seguridad.jpeg'
     },
     {
         name: 'Domotica',
         description: 'Transforma tu hogar con nuestro servicio de instalaciones domóticas. Contamos con una amplia variedad de dispositivos que pueden mejorar tu día a día, brindándote comodidad, eficiencia y control total sobre tu entorno. Desde sistemas de iluminación inteligente hasta termostatos automatizados, personalizamos soluciones para hacer tu vida más fácil y conectada.',
-        image: 'image1.jpg'
+        image: './assets/images/domotica.jpeg'
     },
     {
         name: 'Electricidad Domiciliaria',
         description: 'Optimiza tu hogar con nuestro servicio de electricidad domiciliaria. Ofrecemos soluciones seguras y eficientes para garantizar un suministro eléctrico confiable. Desde instalaciones y reparaciones hasta actualizaciones de sistemas, nuestro equipo experto se encarga de todo. Contamos con las mejores prácticas y tecnologías para asegurar que tu hogar esté siempre conectado de manera segura y eficiente.',
-        image: 'image1.jpg'
+        image: './assets/images/electrica.jpeg'
     },
     {
         name: 'Redes',
         description: 'Garantizamos una conectividad segura y confiable en tu comercio o domicilio mediante tecnología avanzada y cables de alta calidad, asegurando que siempre estés conectado de manera segura.',
-        image: 'image1.jpg'
+        image: './assets/images/redes.jpeg'
     }
 ];
 
@@ -50,12 +50,14 @@ for (let index = 0; index < listServices.length; index++) {
 
 serviceTitle.textContent = listServices[posInit]['name'];
 serviceDescription.textContent = listServices[posInit]['description'];
+
 const listPaginatorUpdate = listPaginator.querySelectorAll('*');
 //counter
 let counter = 1;
 setInterval(() => {
     serviceTitle.textContent = listServices[counter]['name'];
     serviceDescription.textContent = listServices[counter]['description'];
+    serviceImg.setAttribute('src', listServices[counter]['image']);
     listPaginatorUpdate[counter].setAttribute('class','paginator-color-on');
     if (counter === 0) {
         listPaginatorUpdate[listServices.length-1].setAttribute('class','paginator-color-off');
@@ -71,7 +73,6 @@ setInterval(() => {
 
 //function values
 const listCard = document.querySelectorAll(".value");
-console.log(listCard)
 listCard.forEach((card)=>{
     card.addEventListener('mouseover',()=>{
         const img = card.children[0];
@@ -91,6 +92,29 @@ listCard.forEach((card)=>{
         text.classList.add('hidden');
         card.classList.remove('value-background');
     })
+})
+
+//function experience
+const listExperience = document.querySelectorAll('.experiences-item');
+listExperience.forEach((card)=>{
+    card.addEventListener('mouseover',()=>{
+        const img = card.children[0];
+        const title = card.children[1];
+        const text = card.children[2];
+        text.classList.remove('hidden');
+        title.classList.add('hidden');
+        img.classList.add('hidden');
+        console.log('1',img,title)
+    });
+    card.addEventListener('mouseout',()=>{
+        const img = card.children[0];
+        const title = card.children[1];
+        const text = card.children[2];
+        text.classList.add('hidden');
+        title.classList.remove('hidden');
+        img.classList.remove('hidden');
+        console.log('2',img,title)
+    });
 })
 
 window.onscroll = function () {
